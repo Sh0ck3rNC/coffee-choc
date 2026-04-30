49277ad8a869d19a6ad44acd0e0b6a0c9d9f6fb8
Sh0ck3rNC <luca.beroule@gmail.com>
2026-05-01 10:30:39 +1100

    first commit

---
+++ src/main/java/com/miage2026/coffeechoc/CoffeeChocApplication.java
@@ -0,0 +1,13 @@
+package com.miage2026.coffeechoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeChocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeChocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miage2026/coffeechoc/CoffeeChocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage2026.coffeechoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeChocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
